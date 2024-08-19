9c569e7194f8f7c99e8bf8b0be9ed5902b01ff29
miliincode <milena0202@icloud.com>
2024-08-19 21:54:11 +0300

    fix: change width and delete container

--- src/shared/ui/input/index.tsx
+++ src/shared/ui/input/index.tsx
@@ -1,10 +1,6 @@
 import React, { FunctionComponent } from "react";
-import { InputContainer, Input } from "./styles";
+import { Input } from "./styles";
 
 export const InputWidget: FunctionComponent = () => {
-  return (
-    <InputContainer>
-      <Input type="text" placeholder="Введите задачу" />
-    </InputContainer>
-  );
+  return <Input type="text" placeholder="Введите задачу" />;
 };
