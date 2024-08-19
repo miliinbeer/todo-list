29ef6e965a5d5978f3186af0417583e2b8923c48
miliincode <milena0202@icloud.com>
2024-08-19 20:44:25 +0300

    feat: added title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent } from "react";
-import { Root } from "./styles";
-import { GlobalStyles } from "../../app/styles";
+import { Root, Title } from "./styles";
 
 export const Home: FunctionComponent = () => {
-  return <Root>
-  </Root>;
+  return (
+    <Root>
+      <Title>To Do List</Title>
+    </Root>
+  );
 };
