cf5263d2e7fb18335f87fed19856197861299735
miliincode <milena0202@icloud.com>
2024-08-20 16:40:49 +0300

    feat: changed styles

--- src/shared/ui/input/index.tsx
+++ src/shared/ui/input/index.tsx
@@ -1,6 +1,20 @@
 import React, { FunctionComponent } from "react";
+import { InputProps } from "../../types";
 import { Input } from "./styles";
 
-export const InputWidget: FunctionComponent = () => {
-  return <Input type="text" placeholder="Введите задачу" />;
+export const InputWidget: FunctionComponent<InputProps> = ({
+  onChange,
+  onKeyDown,
+  value,
+  type,
+  placeholder,
+}) => {
+  return (
+    <Input
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      type={type}
+      placeholder={placeholder}
+    />
+  );
 };
